80339484ef1e0d17b80b6891b88ac0679f8cf6d0
jgalindo <jgalindo@inszoneins.com>
2026-09-13 19:22:08 -0500

    fix: set trust proxy for express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,7 @@ import registroRoutes from './app/routes/registroRoutes';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
 app.use(express.json());
 
 app.use(
